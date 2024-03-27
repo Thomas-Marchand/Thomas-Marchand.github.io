8b4fada810e630cc3d92e92df3979fa05c89761b
SynthP <thomasmar768@gmail.com>
2024-03-27 12:29:27 +0100

    added first project

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const skillItems = document.querySelectorAll('.skill-item');
-    let isTouchDevice = false;
-
-    window.addEventListener('touchstart', function onFirstTouch() {
-        isTouchDevice = true;
-        window.removeEventListener('touchstart', onFirstTouch, false);
-    }, false);
-
     skillItems.forEach(skillItem => {
         const skillPersonal = skillItem.querySelector('.skill-personal');
 
-        if (!isTouchDevice) {
-            skillItem.addEventListener('mouseenter', function(event) {
-                skillPersonal.style.maxHeight = skillPersonal.scrollHeight + 'px';
-                skillPersonal.classList.add('show');
-            });
+        skillItem.addEventListener('mouseenter', function(event) {
+            skillPersonal.style.maxHeight = skillPersonal.scrollHeight + 'px';
+            skillPersonal.classList.add('show');
+        });
 
-            skillItem.addEventListener('mouseleave', function(event) {
-                skillPersonal.style.maxHeight = '0';
-                skillPersonal.classList.remove('show');
-            });
+        skillItem.addEventListener('mouseleave', function(event) {
+            skillPersonal.style.maxHeight = '0';
+            skillPersonal.classList.remove('show');
+        });
 
-            skillItem.addEventListener('click', toggleSkillPersonal);
-        } else {
-            skillItem.addEventListener('touchend', toggleSkillPersonal);
-        }
-
-        function toggleSkillPersonal(event) {
+        skillItem.addEventListener('click', function(event) {
             event.preventDefault();
             event.stopPropagation();
             if (skillPersonal.classList.contains('show')) {
@@ -36,6 +23,18 @@ document.addEventListener('DOMContentLoaded', function() {
                 skillPersonal.style.maxHeight = skillPersonal.scrollHeight + 'px';
                 skillPersonal.classList.add('show');
             }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const projectItems = document.querySelectorAll('.project-item');
+
+    projectItems.forEach(projectItem => {
+        const img = projectItem.querySelector('img');
+
+        if (img) {
+            projectItem.style.display = 'grid';
         }
     });
 });
